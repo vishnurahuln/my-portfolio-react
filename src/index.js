b29e7d43ae4cd9740966a90c40e2723b85c643b4
Vishnu Rahul <vishnurahuln@gmail.com>
2022-04-08 14:12:13 -0500

    error fix

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { useEffect, useState, useRef } from 'react';
-import BIRDS from './data/birds.json';
-import * as THREE from 'three';
 
 ReactDOM.render(
   <React.StrictMode>
